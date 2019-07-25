b48582dd9052329704f3efbd88de50d0e7ea7813
HuaiXiongWen <565737064@qq.com>
2019-07-25 23:31:22 +0800

    git@github.com:zxf724/--F030.git

---
+++ user/main.c
@@ -0,0 +1,39 @@
+#include "GPIO.h"
+#include "stm32f0xx.h"
+#include "systick.h"
+#include "time.h"
+
+void Go_Zero()  //归零，正转到霍尔中断后反转到霍尔中断
+{
+  uint8_t fre = 2;
+  Speed = 3500;                     //脉冲速度，输出频率
+  Count = 10000;                    //脉冲数，总步数=Count+r2
+  GPIO_SetBits(GPIOA, GPIO_Pin_6);  //方向
+  while (fre) {
+    r1 = 0;
+    r2 = 100;
+    StepCount = 0;
+    GPIO_WriteBit(
+        GPIOA, GPIO_Pin_6,
+        (BitAction)(1 - GPIO_ReadOutputDataBit(GPIOA, GPIO_Pin_6)));  // 翻转
+    TIM_Config();
+    do {
+    } while (r2);
+    fre--;
+    TIM_Cmd(TIM3, DISABLE);
+    Delay(500);
+  }
+}
+
+int main(void) {
+  SystemInit();
+  IO_Init();
+  Systick_Init();
+  GPIO_SetBits(GPIOA, GPIO_Pin_7);  // 使能端
+  Delay(100);
+
+  Go_Zero();
+
+  while (1)
+    ;
+}
